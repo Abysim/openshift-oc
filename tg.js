d9800e69cf978b308264c8aff297bea91f759d20
Abysim <abysim@bigcats.ru>
2016-06-10 20:47:37 +0300

    Logging conversion error

--- tg.js
+++ tg.js
@@ -98,6 +98,7 @@ var serveFile = function(fileId, config, tg, callback) {
                     filePath = newPath;
                     callback(config.httpLocation + '/' + randomString + '/' + path.basename(filePath));
                 } else {
+                    console.error('Convert webp failed: ' + err);
                     var cloudconvert = new (require('cloudconvert'))(config.cloudConvertKey);
                     // create the process. see https://cloudconvert.com/apidoc#create
                     cloudconvert.createProcess({inputformat: 'webp', outputformat: 'png'}, function(err, process) {
